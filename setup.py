daeed0fd6e04a85fa48cf61831fbae7fcf174885
Jakob Mellberg <jakob.mellberg@gmail.com>
2023-07-19 15:51:35 +0200

    Setup.py script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# with open("README.md", "r") as f:
+#     long_description = f.read()
+
+setup(
+    name='spotipy',
+    version='0.0.1',
+    description='A light weight Python library for the Spotify Web API',
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    author="@qwiko",
+    author_email="jakob.mellberg@gmail.com",
+    project_urls={
+        'Source': 'https://github.com/Qwiko/badrum',
+    },
+    install_requires=[
+        "spotipy",
+        "gpiozero",
+    ],
+    license='MIT'
+)
